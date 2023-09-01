7db34882ce988e099dded6d12094d65901b216c0
kadunapikin <ibakoshi27@gmail.com>
2023-09-01 01:05:24 +0100

    add NavBar items and style them

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import '../styles/NavBar.css';
+import logo from '../assets/logo.JPG';
+import { Link } from 'react-scroll';
+import ContactIcon from '../assets/contact.png';
 
 function NavBar() {
   return (
-    <div>NavBar</div>
+    <nav className='navBar'>
+        <img src={logo} alt='logo' className='logo' />
+        <div className='deskstopMenu'>
+          <Link className='deskstopMenuListItem'>Home</Link>
+          <Link className='deskstopMenuListItem'>About</Link>
+          <Link className='deskstopMenuListItem'>Portfolio</Link>
+          <Link className='deskstopMenuListItem'>Clients</Link>
+        </div>
+        <button className='deskstopMenuBtn'>
+          <img src={ContactIcon} alt='contactIcon' className='deskstopMenuImg' />Contact Me</button>
+    </nav>
   )
 }
 
