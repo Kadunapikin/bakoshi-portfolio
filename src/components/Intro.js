fcb6d0df3d3d046e3643e25e8612ab14137ac824
kadunapikin <ibakoshi27@gmail.com>
2023-09-01 03:42:11 +0100

    add intro items and style them

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import '../styles/Intro.css';
+import bg from '../assets/profile.JPG';
+import { Link } from 'react-scroll';
+import hireme from '../assets/hireme.jpg';
+
 
 function Intro() {
   return (
-    <div></div>
+    <section id='intro'>
+      <div className='introContent'>
+        <span className='hello'>Hello,</span>
+        <span className='introText'>I'm <span className='introName'>Bakoshi</span> <br />Front End Web Developer</span>
+        <p className='introPara'>I'm a front end web developer, detail-oriented team player<br/> with strong passion for learning and Creating.</p>
+        <Link><button className='btn'><img src={hireme} alt='Hire me' className='btnImg'/>Hire Me</button></Link>
+      </div>
+      <img src={bg} alt='Profile' className='bg' />
+    </section>
   )
 }
 
