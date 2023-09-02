3de0298b644b57b28738242e59c3303ae75e1f3d
kadunapikin <ibakoshi27@gmail.com>
2023-09-03 00:38:20 +0100

    create Works.js component and Works.css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Intro from "./components/Intro";
 import NavBar from "./components/NavBar";
 import Skills from "./components/Skills";
+import Works from "./components/Works";
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Intro />
       <Skills />
+      <Works />
     </div>
   );
 }
---
+++ src/components/Works.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/Works.css';
+
+function Works() {
+  return (
+    <section id='works'>
+        Works Done!
+    </section>
+  )
+}
+
+export default Works
