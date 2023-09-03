e129b6ec6aa5d149191084b1bab1dcb8332fe2cd
kadunapikin <ibakoshi27@gmail.com>
2023-09-03 09:54:21 +0100

    add items to Works.js

--- src/components/Works.js
+++ src/components/Works.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import '../styles/Works.css';
+import capstone from "../assets/capstone.JPG"
+import etchASketch from '../assets/etchASketch.JPG';
+import portfolio from '../assets/portfolio.JPG';
+import rps from '../assets/rps.JPG';
+import reactPortfolio from '../assets/reactPortfolio.JPG';
+import githubProfile from '../assets/githubProfile.JPG';
 
 function Works() {
   return (
     <section id='works'>
-        Works Done!
+        <h2 className='worksTitle'>My Portfolio</h2>
+        <span worksDesc>I take pride in paying attention to the smallest detail and making sure that my work is pixel perfect. I am excited to bring my skills and experience to help businesses achieve their goals and create a strong online presence.</span>
+        <div className='worksImgs'>
+          <img src={capstone} alt='' className='worksImg' />
+          <img src={etchASketch} alt='' className='worksImg' />
+          <img src={portfolio} alt='' className='worksImg' />
+          <img src={rps} alt='' className='worksImg' />
+          <img src={reactPortfolio} alt='' className='worksImg' />
+          <img src='' alt={githubProfile} className='worksImg' />
+        </div>
     </section>
   )
 }
