481681b419b5e98b1cd8b02f6147390a1fedf301
kadunapikin <ibakoshi27@gmail.com>
2023-09-08 11:19:35 +0100

    create Contact.js and Contact.css

--- src/components/Works.js
+++ src/components/Works.js
@@ -11,7 +11,7 @@ function Works() {
   return (
     <section id='works'>
         <h2 className='worksTitle'>My Portfolio</h2>
-        <span worksDesc>I take pride in paying attention to the smallest detail and making sure that my work is pixel perfect. I am excited to bring my skills and experience to help businesses achieve their goals and create a strong online presence.</span>
+        <span className='worksDesc'>I take pride in paying attention to the smallest detail and making sure that my work is pixel perfect. I am excited to bring my skills and experience to help businesses achieve their goals and create a strong online presence.</span>
         <div className='worksImgs'>
           <img src={capstone} alt='' className='worksImg' />
           <img src={etchASketch} alt='' className='worksImg' />
