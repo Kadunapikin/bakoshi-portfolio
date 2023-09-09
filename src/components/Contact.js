e89e7d8c6797c1cc062190a80d5ebc1552dfa778
kadunapikin <ibakoshi27@gmail.com>
2023-09-09 12:14:24 +0100

    add hamburger icon and add mobile responsiveness to NavBar

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,20 +1,34 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import '../styles/Contact.css';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import GithubIcon from '@mui/icons-material/GitHub';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import InstagramIcon from '@mui/icons-material/Instagram';
+import emailjs from '@emailjs/browser';
 
 function Contact() {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_sewv1wn', 'template_pbg7qwd', form.current, '_7YauwYbf7WyJxckg6dgz')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
+
   return (
     <section id='contactPage'>
         <div id='contact'>
             <h1 className='contactPageTitle'>Contact Me</h1>
             <span className='contactDesc'>Kindly fill out the form bellow to discuss any job opportunity</span>
-            <form className='contactForm'>
-                <input type='text' className='name' placeholder='Your Name' />
-                <input type='email' className='email' placeholder='Your Email' />
+            <form className='contactForm' ref={form} onSubmit={sendEmail}>
+                <input type='text' className='name' placeholder='Your Name' name='your_name' />
+                <input type='email' className='email' placeholder='Your Email' name='your_email' />
                 <textarea className='msg' name='message'rows='5' placeholder='Your Message'></textarea>
                 <button type='submit' value='send' className='submitBtn'>Submit</button>
                 <div className='links'>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/NavBar.css';
 import logo from '../assets/logo.JPG';
 import { Link } from 'react-scroll';
 import ContactIcon from '../assets/contact.png';
+import MenuIcon from '../assets/MenuIcon.JPG';
 
 function NavBar() {
+  const [showMenu, setShowMenu] = useState(false);
+  const handleChange = () => {
+    setShowMenu(false);
+  }
+
   const handleClick = () => {
     document.getElementById('contact').scrollIntoView({behaviour: 'smooth'});
   }
@@ -12,12 +18,21 @@ function NavBar() {
     <nav className='navBar'>
         <img src={logo} alt='logo' className='logo' />
         <div className='deskstopMenu'>
-          <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-100} duration={500} className='deskstopMenuListItem'>Home</Link>
-          <Link activeClass='active' to='skills' spy={true} smooth={true} offset={-50} duration={500} className='deskstopMenuListItem'>About</Link>
-          <Link activeClass='active' to='works' spy={true} smooth={true} offset={-50} duration={500} className='deskstopMenuListItem'>Portfolio</Link>
+          <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-100} duration={500} className='deskstopMenuListItem' onClick={handleChange}>Home</Link>
+          <Link activeClass='active' to='skills' spy={true} smooth={true} offset={-50} duration={500} className='deskstopMenuListItem' onClick={handleChange}>About</Link>
+          <Link activeClass='active' to='works' spy={true} smooth={true} offset={-50} duration={500} className='deskstopMenuListItem' onClick={handleChange}>Portfolio</Link>
         </div>
         <button className='deskstopMenuBtn' onClick={handleClick}>
           <img src={ContactIcon} alt='contactIcon' className='deskstopMenuImg' />Contact Me</button>
+
+          <img src={MenuIcon} alt='menu' className='mobMenu' onClick={()=>setShowMenu(!showMenu)} />
+        <div className='navMenu' style={{display: showMenu? 'flex':'none'}}>
+          <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-100} duration={500} className='ListItem'>Home</Link>
+          <Link activeClass='active' to='skills' spy={true} smooth={true} offset={-50} duration={500} className='ListItem'>About</Link>
+          <Link activeClass='active' to='works' spy={true} smooth={true} offset={-50} duration={500} className='ListItem'>Portfolio</Link>
+          <Link activeClass='active' to='contact' spy={true} smooth={true} offset={-50} duration={500} className='ListItem'>Contact</Link>
+        </div>
+
     </nav>
   )
 }
