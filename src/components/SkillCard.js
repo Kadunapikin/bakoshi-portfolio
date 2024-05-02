68f0b4de7bc2aa8c25192425d633efa4420b2db1
Kadunapikin <ibakoshi27@gmail.com>
2024-05-02 18:00:54 +0100

    add SkillCard.js component

---
+++ src/components/SkillCard.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../styles/SkillCard.css';
+
+const SkillCard = ({ tittle, iconUrl, isActive, onClick}) => {
+  return (
+    <div className={`skills-card ${isActive ? "active" : ""}`}
+    onClick={() => onClick ()}
+    >
+        <div className='skill-icon'>
+            <img src={iconUrl} alt={tittle} />
+        </div>
+        <span>{tittle}</span>
+    </div>
+  )
+}
+
+export default SkillCard
