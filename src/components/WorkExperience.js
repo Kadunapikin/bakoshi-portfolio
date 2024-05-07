ffe9edc19406657a383c09606677d63ed7a0c109
Kadunapikin <ibakoshi27@gmail.com>
2024-05-07 01:04:17 +0100

    add WorkEperience.js file

---
+++ src/components/WorkExperience.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/WorkExperience.css';
+
+
+const WorkExperience = () => {
+  return (
+    <section className='experience-container'>
+        <h5>Work Eperience</h5>
+    </section>
+  )
+}
+
+export default WorkExperience
