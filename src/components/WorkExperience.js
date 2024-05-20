e9ad8081f1e8cba1c778a5c92810c7a717db668a
Kadunapikin <ibakoshi27@gmail.com>
2024-05-20 13:42:30 +0100

    add slider to WorkExperience.js

--- src/components/WorkExperience.js
+++ src/components/WorkExperience.js
@@ -1,19 +1,43 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import '../styles/WorkExperience.css';
 import { WORK_EXPERIENCE } from '../utils/data';
 import ExperienceCard from './ExperienceCard';
+import Slider from 'react-slick';
 
 
 const WorkExperience = () => {
+
+  const sliderRef = useRef();
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 769,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
   return (
     <section className='experience-container'>
         <h5>Work Eperience</h5>
         <div className='experience-content'>
+          <Slider ref={sliderRef} {...settings}>
             {
                 WORK_EXPERIENCE.map((item) => (
                     <ExperienceCard key={item.tittle} details={item} />
                 ))
             }
+          </Slider>
         </div>
     </section>
   )
