f737f3264340004076139bbce5eef90a6f49e0a6
Kadunapikin <ibakoshi27@gmail.com>
2024-10-12 13:07:23 +0100

    add VirtualR app to Works

--- src/components/Works.js
+++ src/components/Works.js
@@ -4,7 +4,7 @@ import capstone from "../assets/capstone.JPG"
 import etchASketch from '../assets/etchASketch.JPG';
 import portfolio from '../assets/portfolio.JPG';
 import rps from '../assets/rps.JPG';
-import reactPortfolio from '../assets/reactPortfolio.JPG';
+import virtualR from '../assets/VirtualR.PNG';
 import responsiveNavBar from '../assets/responsiveNavBar.JPG';
 
 function Works() {
@@ -13,6 +13,10 @@ function Works() {
         <h2 className='worksTitle'>My Portfolio</h2>
         <span className='worksDesc'>I take pride in paying attention to the smallest detail and making sure that my work is pixel perfect. I am excited to bring my skills and experience to help businesses achieve their goals and create a strong online presence.</span>
         <div className='worksImgs'>
+          <div className='app-card'>
+            <img src={virtualR} alt='VirtualR LandingPage' className='worksImg' />
+            <a href='https://virtualr-lilac-seven.vercel.app/'><p>VirtualR LandingPage</p></a>
+          </div>
           <div className='app-card'>
             <img src={capstone} alt='Capstone Project' className='worksImg' />
             <a href='https://phildeoner.github.io/Capstone-Project/'><p>Capstone Project</p></a>
@@ -28,10 +32,6 @@ function Works() {
           <div className='app-card'>
             <img src={rps} alt='Rock Paper and Scissors game' className='worksImg' />
             <a href='https://kadunapikin.github.io/RockPaperScissors/'><p>Rock Paper and Scissors game</p></a>
-          </div>
-          <div className='app-card'>
-            <img src={reactPortfolio} alt='My react Portfolio' className='worksImg' />
-            <a href='https://kadunapikin.github.io/my-react-portfolio/'><p>React Portfolio</p></a>
           </div>
           <div className='app-card'>
             <img src={responsiveNavBar} alt='Responsive NavBar Page' className='worksImg' />
